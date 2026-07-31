ed60f2ff5c8e2fb130b5eb953f49b952a9100d2a
Akhileshwar777 <reddyakhileshwar46@gmail.com>
2026-07-31 15:31:39 +0530

    Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Function to perform Binary Search
+
 int binarySearch(int arr[], int n, int key) {
     int low = 0, high = n - 1;
 
@@ -20,7 +20,7 @@ int binarySearch(int arr[], int n, int key) {
 }
 
 int main() {
-    // Predefined sorted array
+   
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
     int n = sizeof(arr) / sizeof(arr[0]);
 
